c7eb89f683c6b56681a1e9f05054a858a4dcde30
Karem Parra <karem.parra1998@gmail.com>
2021-12-26 02:20:41 -0500

    Primer commit y avance en maquetación del componente signIn

---
+++ src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  formUser = new FormGroup({
+    id: new FormControl('1088035490', Validators.required),
+    password: new FormControl('karemparra', Validators.required)
+  })
+
+}
